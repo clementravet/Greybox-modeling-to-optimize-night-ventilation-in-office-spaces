c77e5507c5b1673b57bea16dc0c394d4928d73a2
clementravet <s243255@dtu.dk>
2026-01-28 18:18:28 +0100

    Update models.py

--- darkgreybox/models.py
+++ darkgreybox/models.py
@@ -2214,7 +2214,8 @@ class TiTmxvCn2R2C_winter_V2(DarkGreyModel):
 
         for k in range(1, num_rec):
             # 1) Radiator heat - direct calculation (no state dynamics)
-            Q_heat[k] = alpha_rad * (MVV[k-1]/100) * (Tfor[k-1] - Ti[k-1])
+            #Q_heat[k] = alpha_rad * (MVV[k-1]/100) * (Tfor[k-1] - Ti[k-1])
+            Q_heat[k] = alpha_rad * (MVV[k-1]/100) * (Tfor[k-1] - Tm[k-1])
 
 
             # 2) Ventilation heat (q_v in m3/h → /3600 for m3/s)
@@ -2232,16 +2233,22 @@ class TiTmxvCn2R2C_winter_V2(DarkGreyModel):
 
 
             # 5) Thermal states
+            #dTi = (
+            #    (Tm[k-1] - Ti[k-1]) / (Rim * Ci)       # Mass→air  
+            #    + (Ta[k-1] - Ti[k-1]) / (Rout * Ci)    # Air→ambient
+            #    + (Q_vent[k] + Q_solar[k] + Q_int[k] + Q_heat[k]) / Ci  # All gains
+            #) * dt
             dTi = (
                 (Tm[k-1] - Ti[k-1]) / (Rim * Ci)       # Mass→air  
                 + (Ta[k-1] - Ti[k-1]) / (Rout * Ci)    # Air→ambient
-                + (Q_vent[k] + Q_solar[k] + Q_int[k] + Q_heat[k]) / Ci  # All gains
+                + (Q_vent[k] + Q_solar[k] + Q_int[k]) / Ci  # All gains
             ) * dt
 
 
-            dTm = (
-                (Ti[k-1] - Tm[k-1]) / (Rim * Cm)       # Air↔mass
-            ) * dt
+            #dTm = (
+            #    (Ti[k-1] - Tm[k-1]) / (Rim * Cm)       # Air↔mass
+            #) * dt
+            dTm = ((Ti[k-1] - Tm[k-1]) / (Rim * Cm) + Q_heat[k] / Cm) * dt
 
 
             Ti[k] = Ti[k-1] + dTi
